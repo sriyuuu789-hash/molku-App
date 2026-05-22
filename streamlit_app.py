bb6ee9bafb0295a4d3e4577606e08f5e3695078e
sriyuuu789-hash <enciuci429@gmail.com>
2026-05-22 10:42:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,15 @@ st.title("Project LPK 2026")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+md = st.text_area('Type in your markdown string (without outer quotes)',
+                  "Happy Streamlit-ing! :balloon:")
+
+st.code(f"""
+import streamlit as st
+
+st.markdown('''{md}''')
+""")
+
+st.markdown(md)
